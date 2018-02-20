28a56f02c263f2fc7a96b216efb07b1af8431794
Arne Brasseur <arne@arnebrasseur.net>
2018-02-20 10:07:38 +0800

    Updates to the repl namespace
    Document what the namespace is for, and add a helper for a one-stop import of
    the demo data.

--- src/clj/clojurians_log/repl.clj
+++ src/clj/clojurians_log/repl.clj
@@ -1,33 +1,69 @@
 (ns clojurians-log.repl
+  "Helper namespace for manual operations like importing stuff. This namespace is
+  not loaded by default, it's mainly useful for devs setting up a local
+  instance, or sys admins seeding the production system with data."
   (:require [clojurians-log.application :as app]
             [clojurians-log.slack-api :as slack]
             [clojurians-log.db.import :as import]
             [clojurians-log.data :as data]
             [clojure.java.io :as io]
-            [datomic.api :as d]))
+            [datomic.api :as d]
+            [clojure.tools.reader.edn :as edn]))
 
-(defn conn []
-  (get-in app/system [:datomic :conn]))
+(defn conn
+  "Reach into the system for the datomic connection.
 
-(defn load-slack-data! []
+  Not 100% kosher but remember this is not production code as such."
+  []
+  (get-in (app/system) [:datomic :conn]))
+
+(defn load-slack-data!
+  "Import Slack users and Channels."
+  []
   (slack/import-users! (conn))
   (slack/import-channels! (conn)))
 
-(defn log-files []
-  (->> (get-in app/config [:slack :log-dir])
-       io/file
-       file-seq
-       (remove #(.isDirectory %))))
+(defn log-files
+  "List all files in the given log directory.
+
+  Defaults to the directory supplied in config.edn."
+  ([]
+   (log-files (get-in app/config [:slack :log-dir])))
+  ([directory]
+   (->> directory
+        io/file
+        file-seq
+        (filter #(re-find #"\.txt$" (str %)))
+        (remove #(.isDirectory %))
+        sort)))
 
 (defn load-log-file! [file]
   (println (str file))
-  (let [msgs (data/event-seq file)]
-    (d/transact (conn) (keep import/event->tx msgs))))
+  (let [msgs (data/event-seq file)
+        events (keep import/event->tx msgs)]
+    (doseq [event events]
+      @(d/transact-async (conn) [event]))))
 
+(defn load-demo-data!
+  "Load the demo data (users, channels, messages).
+
+  First clone https://github.com/plexus/clojurians-log-demo-data, thne call this
+  function, pointing at the repository."
+  [directory]
+  @(d/transact (conn) (edn/read-string (slurp (str directory "/channels.edn"))))
+  (doseq [users (->> "/users.edn"
+                     (str directory)
+                     slurp
+                     edn/read-string
+                     (partition-all 1000))]
+    @(d/transact (conn) users))
+  (run! load-log-file! (log-files directory)))
 
 (comment
   ;; nc localhost 50505
   (use 'clojurians-log.repl)
   (load-slack-data!)
   (run! load-log-file! (log-files))
+
+  (load-demo-data! "/home/arne/github/clojurians-log-demo-data")
   )
