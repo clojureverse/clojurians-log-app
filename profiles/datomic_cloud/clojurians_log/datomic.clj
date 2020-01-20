cdbb7373f97bcc5c6c45965363e561bd7edbe52b
root <root@slackarchiver-1.c.slack-archiver.internal>
2020-01-20 15:57:49 +0000

    Add core.async dependency and wrap transact for cloud

--- profiles/datomic_cloud/clojurians_log/datomic.clj
+++ profiles/datomic_cloud/clojurians_log/datomic.clj
@@ -19,5 +19,6 @@
 (def connect d/connect)
 (def db d/db)
 (def q d/q)
-(def transact d/transact)
-(def transact-async d/transact)
+(def transact (fn [conn data]
+                (d/transact conn {:tx-data data})))
+(def transact-async transact)
