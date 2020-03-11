66ab91b164338f298b98468074d03e4a15e20535
Arne Brasseur <arne@arnebrasseur.net>
2020-03-11 10:15:01 +0100

    Set some clojure indent settings for emacs

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,4 +2,8 @@
          (cider-refresh-after-fn  . "reloaded.repl/resume")
          (cider-clojure-cli-global-options . "-A:dev:datomic-free")
          (cider-preferred-build-tool . clojure-cli)
-         (cider-repl-display-help-banner . nil))))
+         (cider-repl-display-help-banner . nil)))
+ (clojure . ((eval . (define-clojure-indent
+                       (assoc 0)
+                       (for-all 1)
+                       (ex-info 0))))))
