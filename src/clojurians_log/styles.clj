49dd2ba452f950554c7deea88adddbac35b306df
kloimhardt <kloimhardt@kloimhardts-MacBook-Air.local>
2020-03-20 18:49:56 +0100

    move some to styles.css

--- src/clojurians_log/styles.clj
+++ src/clojurians_log/styles.clj
@@ -14,6 +14,39 @@
    :border-radius "20%"})
 
 (defstyles style
+
+  [:html :body :div :span :applet :object :iframe
+   :h1 :h2 :h3 :h4 :h5 :h6 :p :blockquote :pre
+   :a :abbr :acronym :address :big :cite :code
+   :del :dfn :em :img :ins :kbd :q :s :samp
+   :small :strike :strong :sub :sup :tt :var
+   :b :u :i :center
+   :dl :dt :dd :ol :ul :li
+   :fieldset :form :label :legend
+   :table :caption :tbody :tfoot :thead :tr :th :td
+   :article :aside :canvas :details :embed
+   :figure :figcaption :footer :header :hgroup
+   :menu :nav :output :ruby :section :summary
+   :time :mark :audio :video
+   {:margin 0
+    :padding 0
+    :border 0
+    :font "inherit"
+    :font-size "100%"
+    :vertical-align "baseline"}]
+
+  [:html
+   {:line-height 1}]
+
+  [:html
+   {:position "relative"
+    :height "100%"
+    :font-size "16px"
+    :font-family "'Lato', sans-serif"}]
+
+  [:*
+   {:box-sizing "border-box"}]
+
   [:h1 {:text-decoration "underline"}]
 
   [:.day-arrows
