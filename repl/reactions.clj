6cdb5d44df671dc75531bb385acdc901a12a3974
Mitesh Shah <mitesh@miteshshah.com>
2020-12-04 16:14:17 +0530

    chore: repl hacks

--- repl/reactions.clj
+++ repl/reactions.clj
@@ -5,6 +5,7 @@
             [clojurians-log.db.queries :as q]
             [clojurians-log.db.import :as import]
             [clojurians-log.data :as data]
+            [clj-slack.emoji :as slack-emoji]
             [clojure.java.io :as io]
             [clojurians-log.datomic :as d]
             [clojure.tools.reader.edn :as edn]
@@ -15,10 +16,10 @@
 
 (d/q '[:find (pull ?m [* {:reaction/_message [{:reaction/emoji [:emoji/shortcode :emoji/url]}]}])
         :where
-        [?u :user/name "borkdude"]
-        [?m :message/day "2018-02-08"]
+        ;; [?u :user/name "borkdude"]
+        ;; [?m :message/day "2018-02-08"]
         [?m :message/channel ?chan]
-        [?chan :channel/name "aleph"]]
+        [?chan :channel/name "announcements"]]
       (db))
 
 (q/channel-day-messages (db) "aleph" "2018-02-08")
@@ -54,15 +55,30 @@
   (d/transact (conn) [{:emoji/shortcode "+1" :emoji/url "url1"}])
   (d/transact (conn) [{:emoji/shortcode "joy" :emoji/url "url1"}])
   (d/q '[:find (pull ?e [*]) :where [?e :emoji/shortcode]] (db))
+  (d/q '[:find (pull ?e [*]) :where [?e :emoji/shortcode "sheepy"]] (db))
   
   ;; add default emojis
   (def default-emojis
     (with-open [r (io/reader (io/resource "emojis.json"))]
       (let [emoji-list (-> (json/read r :key-fn keyword) :emojis)]
         (map #(hash-map :emoji/shortcode (:name %)) emoji-list))))
+  
+  (def emlist (with-open [r (io/reader (io/resource "emojis.json"))]
+                (let [emoji-list (-> (json/read r :key-fn keyword))]
+                  emoji-list)))
+  
+
+  (into {} (map (comp first #(for [alias (:aliases %)] [alias (:emoji %)])) emlist))
+  
   (d/transact (conn) default-emojis)
   (d/q '[:find (pull ?e [*]) :where [?e :emoji/shortcode]] (db))
   
+  (def emcoll (slack-emoji/list {:api-url "https://slack.com/api"
+   ;; TODO: get rid of this global config access
+                                 :token ""}))
+  
+  (doseq [emojis (partition-all 1000 (:emoji emcoll))]
+    @(d/transact (conn) (mapv import/emoji->tx emojis)))
   
   (load-demo-data! "../clojurians-log-demo-data2")
   )
@@ -83,6 +99,11 @@
                        :reaction/emoji [:emoji/shortcode "+1"]
                        :reaction/ts "1001"
                        :reaction/user [:user/name "plexus"]
-                       :reaction/message [:message/key "C0G922PCH--1518108773.000057"]}]))
+                       :reaction/message [:message/key "C0G922PCH--1518108773.000057"]}])
+  
+  (d/q '[:find (pull ?m [*])
+         :where
+         [?m :message/key "C0G922PCH--1518108773.000057"]]
+       (db)))
 
 
